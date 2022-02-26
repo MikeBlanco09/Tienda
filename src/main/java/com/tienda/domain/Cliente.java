a4483828e26ae0f73998e1ad151c205d72e07601
micha <micha@DESKTOP-0Q46MOE>
2022-02-26 13:51:56 -0600

    Semana 05 - Implementando un CRUD

--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -14,7 +14,7 @@ public class Cliente implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idcliente;
+    private Long idCliente;
     
     private String nombre;
     private String apellidos;
